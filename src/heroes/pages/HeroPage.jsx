d244bed0c0d0035824dfcf04f2410e5bbe8c0704
Adrian Ochoa <adrianfeloh1@gmail.com>
2023-05-31 21:13:37 -0500

    Implementando useMemo y animaciones al HeroCard y al HeroList.

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,10 +1,11 @@
 import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getHeroById } from "../helpers";
+import { useMemo } from "react";
 
 export const HeroPage = () => {
   const { id } = useParams();
 
-  const hero = getHeroById(id);
+  const hero = useMemo( () => getHeroById(id), [ id ]) ;
 
   const navigate = useNavigate();
 
@@ -18,7 +19,7 @@ export const HeroPage = () => {
   } 
 
   return (
-    <div className="row mt-5">
+    <div className="row mt-5 animate__animated animate__fadeIn">
       <div className="col-4">
         <img 
           src={`/assets/heroes/${ id }.jpg`} 
